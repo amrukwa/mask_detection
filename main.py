0d7a1c50e3ce1e585a812abd9cfc2fe1546fc708
amrukwa <a.mrukwa00@gmail.com>
2021-02-24 16:41:58 +0100

    Add some changes to check polyaxon config

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import numpy as np
+import cv2 as cv
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
+# Draw a diagonal blue line with thickness of 5 px
+cv.rectangle(img,(384,0),(510,128),(0,255,0),3)
+cv.circle(img,(447,63), 63, (0,0,255), -1)
+cv.ellipse(img,(256,256),(50,50),0,90,180,255,1)
+
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+if k == ord("s"):
+    cv.imwrite("line.png", img)
