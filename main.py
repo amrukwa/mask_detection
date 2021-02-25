0e0bffd3d38661634ac1baf0179f76560f16e768
amrukwa <a.mrukwa00@gmail.com>
2021-02-25 10:08:36 +0000

    Setup environment (final version)

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import numpy as np
 import cv2 as cv
 # Create a black image
@@ -8,8 +6,8 @@ img = np.zeros((512,512,3), np.uint8)
 cv.rectangle(img,(384,0),(510,128),(0,255,0),3)
 cv.circle(img,(447,63), 63, (0,0,255), -1)
 cv.ellipse(img,(256,256),(50,50),0,90,180,255,1)
-
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
-if k == ord("s"):
-    cv.imwrite("picture.png", img)
+print(1)
+# cv.imshow("Display window", img)
+# k = cv.waitKey(0)
+# if k == ord("s"):
+#     cv.imwrite("picture.png", img)
