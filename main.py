faf3ff53d075efbdc4be2b6a9958c0f54fc86e45
amrukwa <a.mrukwa00@gmail.com>
2021-02-25 09:42:04 +0000

    Check repo

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ cv.rectangle(img,(384,0),(510,128),(0,255,0),3)
 cv.circle(img,(447,63), 63, (0,0,255), -1)
 cv.ellipse(img,(256,256),(50,50),0,90,180,255,1)
 
-# cv.imshow("Display window", img)
-# k = cv.waitKey(0)
-# if k == ord("s"):
-#     cv.imwrite("picture.png", img)
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+if k == ord("s"):
+    cv.imwrite("picture.png", img)
