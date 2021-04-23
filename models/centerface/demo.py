dc030f9a771a0d276e2a45caba349138fbbde0aa
amrukwa <a.mrukwa00@gmail.com>
2021-04-23 14:56:49 +0000

    Run centerface

---
+++ models/centerface/demo.py
@@ -0,0 +1,28 @@
+import cv2
+import scipy.io as sio
+import os
+from centerface import CenterFace
+import sys
+
+def test_image(filename):
+    frame = cv2.imread(filename)
+    h, w = frame.shape[:2]
+    landmarks = True
+    centerface = CenterFace(landmarks=landmarks)
+    if landmarks:
+        dets, lms = centerface(frame, h, w, threshold=0.35)
+    else:
+        dets = centerface(frame, threshold=0.35)
+
+    for det in dets:
+        boxes, score = det[:4], det[4]
+        cv2.rectangle(frame, (int(boxes[0]), int(boxes[1])), (int(boxes[2]), int(boxes[3])), (2, 255, 0), 1)
+    if landmarks:
+        for lm in lms:
+            for i in range(0, 5):
+                cv2.circle(frame, (int(lm[i * 2]), int(lm[i * 2 + 1])), 2, (0, 0, 255), -1)
+    cv2.imwrite('res.png', frame)
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    test_image(filename)
